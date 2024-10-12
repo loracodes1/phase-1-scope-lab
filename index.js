720ef89b540fc58cb4a9ff20512727d3d895c1eb
florence <loracodes3@gmail.com>
2024-10-12 20:02:34 +0300

    a function that when called, declares a variable that declares a variable in the global scope

--- index.js
+++ index.js
@@ -5,3 +5,7 @@ var customerName ="bob"
     return customerName.toUpperCase()
  }
  console.log(upperCaseCustomerName())
+
+ function setBestCustomer (){
+    bestCustomer = "not bob" //variables created without a const, let and var keywords are always globally scoped, regardless of where they sit in your code
+ }
