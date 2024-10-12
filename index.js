b0063375030f351d2263d2c9997b42d32eb9c85f
florence <loracodes3@gmail.com>
2024-10-12 20:06:23 +0300

    overwriting a variable in a global function but now in a function scope

--- index.js
+++ index.js
@@ -9,3 +9,7 @@ var customerName ="bob"
  function setBestCustomer (){
     bestCustomer = "not bob" //variables created without a const, let and var keywords are always globally scoped, regardless of where they sit in your code
  }
+
+ overwriteBestCustomer(){
+    bestCustomer = "maybe bob"
+ }
